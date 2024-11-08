2667162f832fc5972a0452f2a798b0ccdfbd808a
malmans2 <m.almansi@bopen.eu>
2024-11-08 14:33:24 +0100

    revert to _timestamp

--- cads_e2e_tests/cli.py
+++ cads_e2e_tests/cli.py
@@ -61,7 +61,7 @@ def make_reports(
     cache_key: Annotated[
         str,
         Option(help="Key used to invalidate cache"),
-    ] = "nocache",
+    ] = "_timestamp",
 ) -> None:
     """CADS E2E Tests."""
     if requests_path is not None:
