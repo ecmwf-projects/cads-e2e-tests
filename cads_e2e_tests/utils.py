b750e51a19195e926b797ec604bd9c74b2b915af
malmans2 <m.almansi@bopen.eu>
2026-05-15 17:26:51 +0200

    validate forms

--- cads_e2e_tests/utils.py
+++ cads_e2e_tests/utils.py
@@ -119,6 +119,9 @@ def random_range_from_range(
     min_extent: float = 0,
     max_extent: float | None = None,
 ) -> tuple[float, float]:
+    assert start <= stop
+    assert min_extent >= 0
+    assert (stop - start) >= min_extent
     start = random_choice_from_range(start, stop - min_extent, step)
     if max_extent is not None:
         stop = min(stop, start + max_extent)
